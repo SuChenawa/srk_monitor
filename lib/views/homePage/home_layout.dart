246cffc8c50923a855bcf12d1bd413b64e0ae97c
Troy <troyng1302@gmail.com>
2022-08-27 13:52:43 +0800

    inti  flutter_riverpod

---
+++ lib/views/homePage/home_layout.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class HomePageLayout extends StatelessWidget {
+  const HomePageLayout({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('su bao shi 0'),
+      ),
+    );
+  }
+}
