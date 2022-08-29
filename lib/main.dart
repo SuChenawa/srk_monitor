eef3f398a5ffa3e3bea7dade729b237332ac9a31
SuChenawa <wjl1303121277@gmail.com>
2022-08-30 00:41:56 +0800

    delete debug flag

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       color: Colors.transparent,
       title: 'Srk Monitor',
       builder: (context, child) {
