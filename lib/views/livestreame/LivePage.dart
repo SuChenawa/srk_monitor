4fe401be197cef5e3187f33dee27970cbe940cdc
SuChenawa <wjl1303121277@gmail.com>
2022-09-06 22:38:02 +0800

    😭😱😋 超级无敌大更新
    創建了新文件
    這個更新真的有很大嘛(？

---
+++ lib/views/livestreame/LivePage.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LivePage extends StatelessWidget {
+  const LivePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/views/livestreame/widget/LivePageItems.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LivePageItem extends StatelessWidget {
+  const LivePageItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
